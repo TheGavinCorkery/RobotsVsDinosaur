c256516f5f4d818d1af132cf20f36fe0b8f2d432
Gavin Corkery <GavinDCorkery@gmail.com>
2021-09-08 14:43:47 -0400

    Began working on the attack method in dinosaur

--- battlefield.py
+++ battlefield.py
@@ -10,7 +10,21 @@ class Battlefield:
         self.herd = Herd
     
     def run_game(self):
-        pass
+        fleet = Fleet()
+        herd = Herd()
+        robot_one = Robot('Galactus')
+        fleet.create_fleet(robot_one)
+        robot_two = Robot('Terminator')
+        fleet.create_fleet(robot_two)
+        robot_three = Robot('Galaga')
+        fleet.create_fleet(robot_three)
+
+        dino_one = Dinosaur('Teddy', 5)
+        herd.create_herd(dino_one)
+        dino_two = Dinosaur('Ralph', 8)
+        herd.create_herd(dino_two)
+        dino_three = Dinosaur('Hendrix', 5)
+        herd.create_herd(dino_three)
 
     def display_welcome(self):
         pass
